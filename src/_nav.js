8dfc7763f260bcfbc422ced9956692e9ed3c6577
Henry <henrygunawann@gmail.com>
2020-06-09 13:16:25 +0700

    push pertama

---
+++ src/_nav.js
@@ -0,0 +1,169 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+    },
+    {
+      title: true,
+      name: 'MANAGEMENT',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Rules',
+      url: '',
+      icon: 'icon-drop',
+    },
+    {
+      title: true,
+      name: 'OFFERING PROCESS',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Notifs Received',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'Sent to Scoring System',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      name: 'Push Offer',
+      url: '/charts',
+      icon: 'icon-pie-chart',
+    },
+    {
+      title: true,
+      name: 'TAKING OFFERING PROCESS',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Forward Fulfillment to Operator',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'Froward Result to Customer',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      title: true,
+      name: 'DEDUCTION PROCESS',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Topup Notifs Received',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      title: true,
+      name: 'Deduction Request Sent',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Full Repayment',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'Partial Repayment',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      title: true,
+      name: 'SYSTEM HEALTH',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      title: true,
+      name: 'Servers',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'FrontEnd',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'FTP Server',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      name: 'Load Balancer',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      title: true,
+      name: 'Database',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Database',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'Queue System',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+    {
+      title: true,
+      name: 'Connections',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Message Broker',
+      url: '',
+      icon: 'icon-puzzle',
+      
+    },
+    {
+      name: 'Operator',
+      url: '',
+      icon: 'icon-pie-chart',
+    },
+  ]
+};
