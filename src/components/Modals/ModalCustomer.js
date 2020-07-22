3d5df40c7d52e3883514f796e26e09b4a7f3c469
denis212 <shotokhaw213@gmail.com>
2020-07-22 21:36:37 +0700

    fixing datepicker on edit

--- src/components/Modals/ModalCustomer.js
+++ src/components/Modals/ModalCustomer.js
@@ -1,62 +1,80 @@
-import React, { Component } from 'react'
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap'
-import AddEditForm from '../Forms/FormAddEditCustomer'
+import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import AddEditForm from "../Forms/FormAddEditCustomer";
 
 class ModalLoanCustomer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      modal: false
-    }
+      modal: false,
+    };
   }
 
   toggle = () => {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }))
-  }
+    this.setState((prevState) => ({
+      modal: !prevState.modal,
+    }));
+  };
 
   render() {
-      const closeBtn = <button className="close" onClick={this.toggle}>&times;</button>
+    const closeBtn = (
+      <button className="close" onClick={this.toggle}>
+        &times;
+      </button>
+    );
 
-      const label = this.props.buttonLabel
+    const label = this.props.buttonLabel;
 
-      let button = ''
-      let title = ''
+    let button = "";
+    let title = "";
 
-      if(label === 'Edit'){
-        button = <Button
-                  color="warning"
-                  onClick={this.toggle}
-                  style={{float: "left", marginRight:"10px"}}>{label}
-                </Button>
-        title = 'Edit Item'
-      } else {
-        button = <Button
-                  color="success"
-                  onClick={this.toggle}
-                  style={{float: "left", marginRight:"10px"}}>{label}
-                </Button>
-        title = 'Add New Item'
-      }
+    if (label === "Edit") {
+      button = (
+        <Button
+          color="warning"
+          onClick={this.toggle}
+          style={{ float: "left", marginRight: "10px" }}
+        >
+          {label}
+        </Button>
+      );
+      title = "Edit Item";
+    } else {
+      button = (
+        <Button
+          color="success"
+          onClick={this.toggle}
+          style={{ float: "left", marginRight: "10px" }}
+        >
+          {label}
+        </Button>
+      );
+      title = "Add New Item";
+    }
 
-
-      return (
+    return (
       <div>
         {button}
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle} close={closeBtn}>{title}</ModalHeader>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle} close={closeBtn}>
+            {title}
+          </ModalHeader>
           <ModalBody>
             <AddEditForm
               addItemToState={this.props.addItemToState}
               updateState={this.props.updateState}
               toggle={this.toggle}
-              item={this.props.item} />
+              item={this.props.item}
+            />
           </ModalBody>
         </Modal>
       </div>
-    )
+    );
   }
 }
 
-export default ModalLoanCustomer
+export default ModalLoanCustomer;
